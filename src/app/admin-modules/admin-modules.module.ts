5f5c8436d47d30d4c0887e512d63edf9e63263d4
Vos, J.D. (Joeri) <joeri.vos@devolksbank.nl>
2020-10-28 21:28:39 +0100

    Generated modules skeleton.

--- src/app/admin-modules/admin-modules.module.ts
+++ src/app/admin-modules/admin-modules.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { AdminModulesComponent } from './admin-modules.component';
+import { ModulesItemComponent } from './modules-overzicht/modules-item/modules-item.component';
 import { ModulesOverzichtComponent } from './modules-overzicht/modules-overzicht.component';
 import { ModulesToevoegenComponent } from './modules-toevoegen/modules-toevoegen.component';
 
@@ -10,7 +11,8 @@ import { ModulesToevoegenComponent } from './modules-toevoegen/modules-toevoegen
   declarations: [
     AdminModulesComponent,
     ModulesOverzichtComponent,
-    ModulesToevoegenComponent
+    ModulesToevoegenComponent,
+    ModulesItemComponent
   ],
   imports: [
     CommonModule
---
+++ src/app/admin-modules/modules-overzicht/modules-item/modules-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modules-item',
+  templateUrl: './modules-item.component.html',
+  styleUrls: ['./modules-item.component.css']
+})
+export class ModulesItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
