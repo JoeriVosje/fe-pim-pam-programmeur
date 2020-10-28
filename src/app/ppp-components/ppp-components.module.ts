9635baed38e314ceb99cc897003c623d7ffdcd0c
Vos, J.D. (Joeri) <joeri.vos@devolksbank.nl>
2020-10-28 20:44:45 +0100

    Tslint rule and comment.

--- src/app/ppp-components/ppp-components.module.ts
+++ src/app/ppp-components/ppp-components.module.ts
@@ -1,8 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
-
-
+/**
+ * In plaats van een aparte library is deze module aangemaakt.
+ * Hier kunnen de verschillende gedeelde componenten in worden
+ * aangemaakt zoals een button, input of het overview template.
+ */
 @NgModule({
   declarations: [],
   imports: [
