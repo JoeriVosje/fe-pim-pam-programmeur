4221be00dd981571e4fd141492eb584466c2c01d
robinbastiaan <robinbastiaan@gmail.com>
2020-10-29 20:23:26 +0100

    Add button with styles

--- src/app/ppp-components/ppp-components.module.ts
+++ src/app/ppp-components/ppp-components.module.ts
@@ -1,13 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { ButtonComponent } from './button/button.component';
+
 /**
  * In plaats van een aparte library is deze module aangemaakt.
  * Hier kunnen de verschillende gedeelde componenten in worden
  * aangemaakt zoals een button, input of het overview template.
  */
 @NgModule({
-  declarations: [],
+  declarations: [ButtonComponent],
+  exports: [
+    ButtonComponent
+  ],
   imports: [
     CommonModule
   ]
