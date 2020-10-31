1eb0a1043138ff347ce51fa528b2d6acf4dd1856
sarcode <sarkout.mahmoud@rabobank.nl>
2020-10-31 13:17:04 +0100

    1.8 button toegevoegd

--- src/app/admin-modules/admin-modules.module.ts
+++ src/app/admin-modules/admin-modules.module.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { PppComponentsModule } from '../ppp-components/ppp-components.module';
 import { AdminModulesComponent } from './admin-modules.component';
 import { ModulesItemComponent } from './modules-overzicht/modules-item/modules-item.component';
 import { ModulesOverzichtComponent } from './modules-overzicht/modules-overzicht.component';
 import { ModulesToevoegenComponent } from './modules-toevoegen/modules-toevoegen.component';
-import { PppComponentsModule } from '../ppp-components/ppp-components.module';
 
 
 @NgModule({
