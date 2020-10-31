dd6da2dd6fb7c421c623d3fc5971fa7374c31e2e
sarcode <sarkout.mahmoud@rabobank.nl>
2020-10-31 13:15:49 +0100

    1.8 button toegevoegd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AdminModulesModule } from './admin-modules/admin-modules.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PppComponentsModule } from './ppp-components/ppp-components.module';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
