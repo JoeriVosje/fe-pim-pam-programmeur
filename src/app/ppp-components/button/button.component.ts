189f371ec3037139f8aac71dcfa444d5eab01695
robinbastiaan <robinbastiaan@gmail.com>
2020-11-05 20:01:55 +0100

    Try out on button text

--- src/app/ppp-components/button/button.component.ts
+++ src/app/ppp-components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./button.component.css']
 })
 export class ButtonComponent implements OnInit {
+  @Input()
+  customTitle: string;
 
   constructor() { }
 
   ngOnInit(): void {
+    // set the button text
   }
 
 }
