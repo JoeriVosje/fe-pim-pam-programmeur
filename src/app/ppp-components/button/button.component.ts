774eccc3e9c083bae0e7c0b5f82f18075fdc1fad
RobinBastiaan <robinbastiaan@gmail.com>
2020-11-07 11:09:35 +0100

    Rename to ppp-button

--- src/app/ppp-components/button/button.component.ts
+++ src/app/ppp-components/button/button.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-button',
+  selector: 'ppp-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.css']
 })
