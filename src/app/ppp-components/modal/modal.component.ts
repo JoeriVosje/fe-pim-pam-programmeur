217388db0f2faa72210b28f6773c552dc4f56c34
Linda Heine <17134811@student.hhs.nl>
2020-11-08 20:25:30 +0100

    klooien met layout

--- src/app/ppp-components/modal/modal.component.ts
+++ src/app/ppp-components/modal/modal.component.ts
@@ -8,18 +8,19 @@ import { MatDialog, MatDialogRef, MAT_DIALOG_DATA, } from '@angular/material/dia
 
 export class ModalComponent {
 
-  @Input() texts:any
-  @Output() eventClick= new EventEmitter()
+  @Input() texts: any
+  @Output() eventClick = new EventEmitter()
 
-  constructor(public dialog: MatDialog) {}
+  constructor(public dialog: MatDialog) { }
 
   openDialog(): void {
 
     const dialogRef = this.dialog.open(ModalComponentDialog, {
-      width: '300px',
+      width: '400px',
       data: this.texts,
       disableClose: true,
-      autoFocus: true
+      autoFocus: true,
+      panelClass: 'ppp-modalbox'
     });
 
     dialogRef.afterClosed().subscribe(result => {
@@ -30,16 +31,34 @@ export class ModalComponent {
 
 @Component({
   selector: 'ppp-modaldialog',
-  templateUrl: './modaldialog.component.html'
+  templateUrl: './modaldialog.component.html',
+  styles: [`
+    #modal-button {
+      color: #f53c56;
+      background-color: white;
+      border: none;
+      border-radius: 5px;
+      padding: 10px 32px;
+      text-align: center;
+      font-size: 16px;
+      margin: 8px;
+      width: 110px;
+      height: 40px;
+      },
+      #modal-button:hover {
+        cursor: pointer;
+        filter: brightness(80%);
+      }
+    `]
 })
 
 export class ModalComponentDialog implements OnInit {
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: {
-                  confirmText: string,
-                  message: string,
-                  title: string
-              }, public dialogRef: MatDialogRef<ModalComponentDialog>){}
+    confirmText: string,
+    message: string,
+    title: string
+  }, public dialogRef: MatDialogRef<ModalComponentDialog>) { }
 
   close(): void {
     this.dialogRef.close('modal gesloten');
