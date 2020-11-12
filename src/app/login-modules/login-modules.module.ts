f37a18ddbf910b613d3426cd719d932ae756ebf0
robinbastiaan <robinbastiaan@gmail.com>
2020-11-12 20:47:56 +0100

    Add routing to LoginModulesComponent

--- src/app/login-modules/login-modules.module.ts
+++ src/app/login-modules/login-modules.module.ts
@@ -1,16 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { LoginModulesComponent } from './login-modules.component';
 
+// path declare
+const routes: Routes = [
+  {path: 'login', component: LoginModulesComponent, data: {}}
+];
 
 @NgModule({
   declarations: [
     LoginModulesComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ],
   exports: [LoginModulesComponent]
 })
-export class LoginModulesModule { }
+export class LoginModulesModule {
+}
+
