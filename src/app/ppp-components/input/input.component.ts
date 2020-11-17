d5b57bad53289661a2a8a7a06770c1bce727e389
Brian Manuputty <bbmanuputty@gmail.com>
2020-11-17 16:01:18 +0100

    Remove control value accessor original input

--- src/app/ppp-components/input/input.component.ts
+++ src/app/ppp-components/input/input.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { ControlValueAccessor, FormControl } from '@angular/forms';
+import { FormControl } from '@angular/forms';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -7,7 +7,7 @@ import { Subscription } from 'rxjs';
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.css']
 })
-export class InputComponent implements OnInit, ControlValueAccessor {
+export class InputComponent implements OnInit {
   @Input() public size: string;
   @Input() public id: string;
   @Input() public value = '';
@@ -30,37 +30,4 @@ export class InputComponent implements OnInit, ControlValueAccessor {
       this.inputFieldChange.emit(this.inputField.value);
     });
   }
-
-  /**
-   * Write form value to the DOM element (model => view)
-   */
-  writeValue(value: any): void {
-    this.value = value;
-  }
-
-  /**
-   * Write form disabled state to the DOM element (model => view)
-   */
-  setDisabledState(isDisabled: boolean): void {
-    this.disabled = isDisabled;
-  }
-
-  /**
-   * Update form when DOM element value changes (view => model)
-   */
-  registerOnChange(fn: any): void {
-    // Store the provided function as an internal method.
-    this.onChange = fn;
-  }
-
-  /**
-   * Update form when DOM element is blurred (view => model)
-   */
-  registerOnTouched(fn: any): void {
-    // Store the provided function as an internal method.
-  }
-
-  public onChange(value: any): void {
-  }
-
 }
